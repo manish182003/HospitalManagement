9bbfc78a7a0ca5e395e6237df0382a02298d3ed4
Manish Joshi <manishjoshi182003@gmail.com>
2025-05-16 13:22:03 +0530

    fixed done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -35,22 +35,28 @@ export const login = async (req, res) => {
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ msg: "User is not Yet Registered." });
+      return res
+        .status(400)
+        .json({ success: false, message: "User is not Yet Registered." });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+    if (!isMatch)
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "7d",
     });
 
     res.json({
+      success: true,
       token,
       user: { id: user._id, name: user.name, email: user.email },
     });
   } catch (err) {
-    res.status(500).json({ msg: "Server error" });
+    res.status(500).json({ success: true, message: "Server error" });
   }
 };
 
