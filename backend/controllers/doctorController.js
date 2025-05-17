c51c930234e1be51a47e1329ee6e03d4da2785be
Narendra Singh <nsdanu707@gmail.com>
2025-05-17 22:14:03 +0530

    Update doctorController.js

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -181,35 +181,21 @@ const changeAvailablity = async (req, res) => {
 //by Narendra------------------------------------------------------------
 const doctorDashboard = async (req, res) => {
   try {
-    const docId = req.user.id;
-
+    const { docId } = req.body;
     const appointments = await appointmentModel.find({ docId });
-
     let earnings = 0;
-    let patients = [];
 
-    appointments.forEach((item) => {
+    appointments.map((item) => {
       if (item.isCompleted || item.payment) {
         earnings += item.amount;
       }
+    });
+    let patients = [];
+    appointments.map((item) => {
       if (!patients.includes(item.userId)) {
         patients.push(item.userId);
       }
     });
-
-    const dashData = {
-      appointments: appointments.length,
-      earnings,
-      patients: patients.length,
-      latestAppointments: appointments.reverse().slice(0, 5),
-    };
-
-    return res.json({ success: true, dashData });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, message: error.message });
-  }
-};
 
 
 export { getDoctorAppointments, doctorLogin, changeAvailablity, doctorDashboard };
