2bbe78701cfa9f0bc955dbd463c1e1f0e09b1c6b
Narendra Singh <nsdanu707@gmail.com>
2025-05-17 14:35:55 +0530

    Update adminRoute.js

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -9,6 +9,7 @@ import {
 } from "../controllers/adminController.js";
 import upload from "../middlewares/multer.js";
 import authAdmin from "../middlewares/authAdmin.js";
+import { changeAvailablity } from "../controllers/doctorController.js";
 
 const adminRouter = express.Router();
 
@@ -20,5 +21,6 @@ adminRouter.delete("/remove-doctor/:doctorId", authAdmin, removeDoctor);
 adminRouter.get("/dashboard", authAdmin, getAdminDashboard);
 adminRouter.get("/all/doc", authAdmin, getAllDoctors); 
 adminRouter.get("/all-appointments",authAdmin,getAllAppointments)
+adminRouter.post("/change-availability", authAdmin, changeAvailablity); //by narendra
 
 export default adminRouter;
