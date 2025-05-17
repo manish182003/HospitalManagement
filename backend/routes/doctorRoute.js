0d01e842e907ba838147d87c6ba638b4418737a9
Narendra Singh <nsdanu707@gmail.com>
2025-05-17 23:16:37 +0530

    Update doctorRoute.js

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -41,7 +41,7 @@ router.get("/speciality/:speciality", getDoctorsBySpeciality);
 
 //by Narendra------------------------------------------------------
 router.get("/doctor-dashboard", authDoctor, doctorDashboard);
-router.get("/appointments", authDoctor, appointmentsDocoter);
+router.get("/appointments", authDoctor, getDoctorAppointments);
 
 
 // Assign nurse
