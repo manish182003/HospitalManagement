f94729d94972078649c380fc22b03435977118a3
Nru707 <nsdanu707@gmail.com>
2025-05-17 22:41:02 +0530

    "commit"

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,9 +2,11 @@ import { useContext } from "react";
 import { AdminAppContext } from "../context/AdminAppContext";
 import { NavLink } from "react-router-dom";
 import { assets } from "../assets/assets";
+import { DoctorContext } from "../context/DoctorContext";
 
 const Sidebar = () => {
   const { aToken } = useContext(AdminAppContext);
+  const { dToken } = useContext(DoctorContext);
   return (
     <div className="min-h-screen bg-white">
       {aToken && (
@@ -12,7 +14,9 @@ const Sidebar = () => {
           <NavLink
             className={({ isActive }) =>
               `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
-                isActive ? "bg-gray-100 border-r-4 border-black rounded-md " : ""
+                isActive
+                  ? "bg-gray-100 border-r-4 border-black rounded-md "
+                  : ""
               }`
             }
             to={"/admin-dashboard"}
@@ -23,7 +27,9 @@ const Sidebar = () => {
           <NavLink
             className={({ isActive }) =>
               `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
-                isActive ? "bg-gray-100 border-r-4 border-black rounded-md " : ""
+                isActive
+                  ? "bg-gray-100 border-r-4 border-black rounded-md "
+                  : ""
               }`
             }
             to={"/all-appointment"}
@@ -34,7 +40,9 @@ const Sidebar = () => {
           <NavLink
             className={({ isActive }) =>
               `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
-                isActive ? "bg-gray-100 border-r-4 border-black rounded-md " : ""
+                isActive
+                  ? "bg-gray-100 border-r-4 border-black rounded-md "
+                  : ""
               }`
             }
             to={"/add-doctor"}
@@ -45,13 +53,58 @@ const Sidebar = () => {
           <NavLink
             className={({ isActive }) =>
               `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
-                isActive ? "bg-gray-100 border-r-4 border-black rounded-md " : ""
+                isActive
+                  ? "bg-gray-100 border-r-4 border-black rounded-md "
+                  : ""
               }`
             }
             to={"/doctor-list"}
           >
             <img src={assets.people_icon} alt="" />
             <p>Doctor List</p>
+          </NavLink>
+        </ul>
+      )}
+      {dToken && (
+        <ul className="text-gray-600 mt-4">
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
+                isActive
+                  ? "bg-gray-100 border-r-4 border-black rounded-md "
+                  : ""
+              }`
+            }
+            to={"/doctor-dashboard"}
+          >
+            <img src={assets.home_icon} alt="" />
+            <p className="hidden md:block">Dashboard</p>
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
+                isActive
+                  ? "bg-gray-100 border-r-4 border-black rounded-md "
+                  : ""
+              }`
+            }
+            to={"/doctor-appointment"}
+          >
+            <img src={assets.appointment_icon} alt="" />
+            <p className="hidden md:block">Appointment</p>
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
+                isActive
+                  ? "bg-gray-100 border-r-4 border-black rounded-md "
+                  : ""
+              }`
+            }
+            to={"/doctor-profile"}
+          >
+            <img src={assets.people_icon} alt="" />
+            <p className="hidden md:block">Profile</p>
           </NavLink>
         </ul>
       )}
