dc2b5d6f685772290322a2e9ba2759454d58af2c
Nru707 <nsdanu707@gmail.com>
2025-05-17 22:34:05 +0530

    "commit"

--- src/context/DoctorContext.jsx
+++ src/context/DoctorContext.jsx
@@ -68,11 +68,11 @@ const DoctorContextProvider = (props) => {
 
   const getDashData = async () => {
     try {
-      const { data } = await axios.get(`${backendUrl}api/doctor/dashboard`, {
-        headers: {
-          Authorization: `Bearer ${dToken}`,
-        },
-      });
+      const { data } = await axios.get(`${backendUrl}api/doctor/doctor-dashboard`, {
+  headers: {
+    Authorization: `Bearer ${dToken}`,
+  },
+});
 
       if (data.success) {
         setDashData(data.dashData);
