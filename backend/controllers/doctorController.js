22a8f9ac0bac9034eb2d7b6577cc9c116eb98e14
Prince Joshi <129651719+PrinceJoshi312@users.noreply.github.com>
2025-05-18 21:48:52 +0530

    Update doctorController.js

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -194,6 +194,20 @@ const doctorDashboard = async (req, res) => {
         patients.push(item.userId);
       }
     });
+// ----------------all doctors----------------
+export const getAllDoctors = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find().populate("available_slots"); // Fetch all doctors
+
+    res.status(200).json({ success: true, data: doctors });
+  } catch (error) {
+    console.error(error);
+
+    res
+      .status(500)
+      .json({ success: false, message: "Failed to fetch doctors" });
+  }
+};
 
 
 export {  doctorLogin,
