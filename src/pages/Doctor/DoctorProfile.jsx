5866172dba661131c7123679ebf0b0d0fd7b9569
Nru707 <nsdanu707@gmail.com>
2025-05-18 22:07:25 +0530

    "commit"

--- src/pages/Doctor/DoctorProfile.jsx
+++ src/pages/Doctor/DoctorProfile.jsx
@@ -21,7 +21,7 @@ const DoctorProfile = () => {
       };
 
       const { data } = await axios.post(
-        backendUrl + "/api/doctor/update-profile",
+        backendUrl + "api/doctor/update-profile",
         updateData,
         { headers: { dToken } }
       );
