521b2d64e2d89660b31d20c9927a87617c61f95b
Manish Joshi <manishjoshi182003@gmail.com>
2025-05-19 20:11:59 +0530

    fixed

--- backend/cron/updateAppointmentStatus.js
+++ backend/cron/updateAppointmentStatus.js
@@ -5,40 +5,34 @@ import moment from "moment-timezone";
 export const updateAppointmentStatus = () => {
   cron.schedule("*/1 * * * *", async () => {
     console.log("Running Cron Job to Update Appointment Status...");
+
     try {
-      //   const today = new Date();
+      const startOfTodayKolkata = moment.tz("Asia/Kolkata").startOf("day");
+      const endOfTodayKolkata = moment.tz("Asia/Kolkata").endOf("day");
 
-      //   console.log(
-      //     "Today (in Asia/Kolkata timezone, midnight):",
-      //     today.getUTCDate()
-      //   );
+      const startOfTodayUTC = startOfTodayKolkata.clone().utc().toDate();
+      const endOfTodayUTC = endOfTodayKolkata.clone().utc().toDate();
 
-      const todayInKolkata = moment
-        .tz("Asia/Kolkata")
-        .add(1, "day")
-        .startOf("day")
-        .clone()
-        .utc();
-      console.log(
-        "Today (in Asia/Kolkata timezone, midnight):",
-        todayInKolkata.toDate()
-      );
+      console.log("Today Start (UTC):", startOfTodayUTC);
+      console.log("Today End (UTC):", endOfTodayUTC);
 
+      // Update to 'Completed' if before today's start
       await appointmentModel.updateMany(
-        { date: { $lt: todayInKolkata.toDate() } },
+        { date: { $lt: startOfTodayUTC } },
         { $set: { status: "Completed" } },
         { runValidators: true }
       );
 
+      // Update to 'Ongoing' if within today (from midnight to 11:59:59 PM)
       await appointmentModel.updateMany(
-        { date: todayInKolkata.toDate() },
+        { date: { $gte: startOfTodayUTC, $lte: endOfTodayUTC } },
         { $set: { status: "Ongoing" } },
         { runValidators: true }
       );
 
-      console.log("Appointment Updated Successfully...");
+      console.log("Appointment statuses updated successfully...");
     } catch (error) {
-      console.error("error in updating appointment status", error);
+      console.error("Error updating appointment status:", error);
     }
   });
 };
