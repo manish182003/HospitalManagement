c68240a7fc3b7afa0130e5dbf0a565604e85faa7
Manish Joshi <manishjoshi182003@gmail.com>
2025-05-19 17:38:14 +0530

    fixed

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -174,7 +174,7 @@ export const refund = async (req, res) => {
       return res.status(400).json({
         success: false,
         message: "Refund failed. Please contact support.",
-        error: error.message,
+        // error: error.message,
       });
     }
   } catch (error) {
